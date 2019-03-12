6fd87a864c30cb0018af177d9afeab72242cffda
Davide Santangelo <davide.santangelo@gmail.com>
2019-03-12 11:50:41 +0100

    cleanup

--- spec/apiwha_spec.rb
+++ spec/apiwha_spec.rb
@@ -14,8 +14,15 @@ RSpec.describe Apiwha do
     end
   end
 
+  it 'send message' do
+    VCR.use_cassette 'send_message' do
+      result = Apiwha.new(api_key).send_message(number: '5491133381736', text: 'hello dude!')
+      expect(result).not_to be_empty
+    end
+  end
+
   it 'get credit' do
-    VCR.use_cassette 'pull_messages' do
+    VCR.use_cassette 'get_credit' do
       result = Apiwha.new(api_key).get_credit
       expect(result).not_to be_empty
       expect(result['credit']).to be >= 0
